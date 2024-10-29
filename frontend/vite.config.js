8fb2e89ae0e8d8dc60779b87cf9b667f8bff31d1
Ganesh Srambikal <ganesh.srambikal@gmail.com>
2024-10-29 16:31:47 +0530

    addes build  vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,9 +7,9 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // build: {
-  //   outDir: 'dist', // ensures the build output goes to 'dist'
-  // },
+  build: {
+    outDir: 'dist', // ensures the build output goes to 'dist'
+  },
   base: '/',
   // server: {
   //   proxy: {
