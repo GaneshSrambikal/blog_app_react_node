34866f7172167c98f26dc796bc34d7112493845c
Ganesh Srambikal <ganesh.srambikal@gmail.com>
2024-11-11 23:21:30 +0530

    fixes imports in homepage

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -8,7 +8,7 @@ import HeroBlogCard from '../components/blogs/HeroBlogCard';
 import Carousel from '../components/ui/Carousel';
 import { FaSearch } from 'react-icons/fa';
 import { Navigate } from 'react-router-dom';
-import HomeBlogCard from '../components/blogs/HeroBlogCard';
+import HomeBlogCard from '../components/blogs/homeBlogCard';
 
 const react_base_url = import.meta.env.VITE_API_BASE_URL;
 const Home = () => {
