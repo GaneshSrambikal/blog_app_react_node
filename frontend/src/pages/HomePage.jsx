f422811db7b68dcafd72466d8a71b39fe03f8e2b
Ganesh Srambikal <ganesh.srambikal@gmail.com>
2024-11-11 23:48:17 +0530

    changes comps names to fixes casing error

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -8,7 +8,7 @@ import HeroBlogCard from '../components/blogs/HeroBlogCard';
 import Carousel from '../components/ui/Carousel';
 import { FaSearch } from 'react-icons/fa';
 import { Navigate } from 'react-router-dom';
-import HomeBlogCard from '../components/blogs/homeBlogCard';
+import FilterCard from '../components/blogs/FilterCard';
 
 const react_base_url = import.meta.env.VITE_API_BASE_URL;
 const Home = () => {
@@ -75,7 +75,7 @@ const Home = () => {
             {blogs &&
               blogs
                 .reverse()
-                .map((blog, index) => <HomeBlogCard key={index} blog={blog} />)}
+                .map((blog, index) => <FilterCard key={index} blog={blog} />)}
           </div>
         </div>
 
